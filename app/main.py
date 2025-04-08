9f544cb7070a2e44e785249831f3905ef26c124c
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-04-08 11:16:56 -0300

    Implements automatic final confirm for reverse logistic service

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
+from app.config import config
 from app.correios.login import CorreiosLogin
 from app.correios.receiver_form import ReceiverForm
 from app.correios.sender_form import SenderForm
@@ -24,9 +27,18 @@ def main():
     sender_form: SenderForm = SenderForm(driver)
     sender_form.fill()
 
-    # Automatic Confirm
-    # generate_logistic_button = driver.find_element(By.NAME, "btnEnviar")
-    # generate_logistic_button.click()
-    # config.sleep()
+    generate_logistic_button: WebElement = driver.find_element(
+        By.NAME, "btnEnviar"
+    )
+    generate_logistic_button.click()
+    config.sleep()
+    driver.switch_to.alert.accept()
+    config.sleep()
+
+    # # Gets final reverse logistic post code
+    # authorization_post_code: str | None = driver\
+    #     .find_element(By.CSS_SELECTOR, 'font[color="red"] > b')\
+    #     .get_attribute("innerText")
+    # print(authorization_post_code)
 
     input("Pressione enter para encerrar")
